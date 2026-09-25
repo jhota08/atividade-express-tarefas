8db3b9007e1d0bfbbab8f685a726e151265091e7
jhota08 <batistajuliaoj@gmail.com>
2026-09-24 20:37:44 -0400

    feat: adiciona rota de tarefas

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 
 const app = express();
+const tarefas =[
+    {id:1, titulo:'Estudar JavaScript', concluida: false},
+    {id:2, titulo:'Fazer atividade de Express', concluida:true},
+    {id:3, titulo: 'Entregar trabalho', concluida: false}
+]
 
 app.get('/', (req, res) =>{
     res.send('API de Tarefas no ar')
+});
+
+app.get('/tarefas', (req, res) =>{
+    res.json(tarefas)
 });
 
 app.listen(3000, () =>{
     console.log('Servidor rodando na porta 3000')
 
 });
+
